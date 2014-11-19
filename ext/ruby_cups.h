46fc1a0d454f35637036fede47b5c5fb937bef74
Issac Gerges <igerges@atlassian.com>
2014-11-19 17:46:09 +1100

    yosemite fix

--- ext/ruby_cups.h
+++ ext/ruby_cups.h
@@ -1,8 +1,9 @@
+#define _IPP_PRIVATE_STRUCTURES 1
 #include <cups/cups.h>
 
 // st.h is needed for ST_CONTINUE constant
 #include <ruby.h>
-#include <st.h>
+#include <ruby/st.h>
 
 #ifndef MAXOPTS
   #define MAXOPTS 100
